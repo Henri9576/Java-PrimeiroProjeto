4f40a772bffc61aac5a9cef897c14a6e51959517
henriquekill9576 <46168172+henriquekill9576@users.noreply.github.com>
2023-10-22 17:13:17 -0300

    Adicionando projeto

---
+++ app/src/main/java/Aula4CMeuPrimeiroProjeto/App.java
@@ -0,0 +1,21 @@
+package Aula4CMeuPrimeiroProjeto;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        //Comentário de linha
+        
+        /* 
+        Minha primeira linha de comentário
+        Segunda linha de comentário
+        */
+        
+        /** 
+        Comentário de documentação
+        */
+        
+        System.out.println("Olá mundo");
+        System.out.println("Meu segundo comando de saída.");
+    }
+}
